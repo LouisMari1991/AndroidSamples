c82ed56b2b0f828bccad37b64be895a2eac319cf
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2016-08-19 18:51:43 +0800

    trainning leakCanary,BlockCanary

--- app/src/main/java/com/sync/androidsamples/SampleApplication.java
+++ app/src/main/java/com/sync/androidsamples/SampleApplication.java
@@ -1,8 +1,10 @@
 package com.sync.androidsamples;
 
 import android.app.Application;
+import android.content.Context;
 import com.github.moduth.blockcanary.BlockCanary;
 import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 import com.sync.androidsamples.common.AppBlockCanaryContext;
 
 /**
@@ -10,9 +12,17 @@ import com.sync.androidsamples.common.AppBlockCanaryContext;
  */
 public class SampleApplication extends Application {
 
+
+  public static RefWatcher getRefWatcher(Context context) {
+    SampleApplication application = (SampleApplication) context.getApplicationContext();
+    return application.refWatcher;
+  }
+
+  private RefWatcher refWatcher;
+
   @Override public void onCreate() {
     super.onCreate();
-    LeakCanary.install(this);
+    refWatcher = LeakCanary.install(this);
 
     // Do it on main process
     BlockCanary.install(this, new AppBlockCanaryContext()).start();
--- app/src/main/java/com/sync/androidsamples/common/BaseActivity.java
+++ app/src/main/java/com/sync/androidsamples/common/BaseActivity.java
@@ -27,5 +27,4 @@ public class BaseActivity  extends AppCompatActivity {
     }
   }
 
-
 }
