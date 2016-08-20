f60347be2d91d6b878d03b0b7552eb998f0efcb0
“youMarishunxiang@gmail.com <mousefly100%>
2016-08-20 17:40:21 +0800

    new class

---
+++ app/src/main/java/com/sync/androidsamples/google/samples/apps/topeka/fragment/CategorySelectionFragment.java
@@ -0,0 +1,8 @@
+package com.sync.androidsamples.google.samples.apps.topeka.fragment;
+
+/**
+ * Author：Administrator on 2016/8/20 0020 17:40
+ * Contact：289168296@qq.com
+ */
+public class CategorySelectionFragment {
+}
---
+++ app/src/main/java/com/sync/androidsamples/google/samples/apps/topeka/helper/JsonHelper.java
@@ -0,0 +1,8 @@
+package com.sync.androidsamples.google.samples.apps.topeka.helper;
+
+/**
+ * Author：Administrator on 2016/8/20 0020 17:38
+ * Contact：289168296@qq.com
+ */
+public class JsonHelper {
+}
