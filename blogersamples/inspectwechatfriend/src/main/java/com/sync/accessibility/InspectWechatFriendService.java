b7295fc35846678e41f74410b514986e2518fd03
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2016-10-27 09:30:35 +0800

    add module inspectwechatfriend 2016/10/27

---
+++ blogersamples/inspectwechatfriend/src/main/java/com/sync/accessibility/InspectWechatFriendService.java
@@ -0,0 +1,19 @@
+package com.sync.accessibility;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by YH on 2016-10-27.
+ */
+
+public class InspectWechatFriendService extends AccessibilityService {
+
+  @Override public void onAccessibilityEvent(AccessibilityEvent event) {
+
+  }
+
+  @Override public void onInterrupt() {
+    
+  }
+}
---
+++ blogersamples/inspectwechatfriend/src/main/java/com/sync/inspectwechatfriend/activity/DeleteFriendListActivity.java
@@ -0,0 +1,19 @@
+package com.sync.inspectwechatfriend.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.sync.inspectwechatfriend.R;
+
+/**
+ * Created by YH on 2016-10-27.
+ */
+
+public class DeleteFriendListActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_delete_friend_list);
+
+  }
+}
