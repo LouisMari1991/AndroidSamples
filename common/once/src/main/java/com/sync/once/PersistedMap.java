ad9da52127f179124754763388bd9a8218c2a16b
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2016-11-03 11:15:43 +0800

    init Once by sync on 2016/11/03

---
+++ common/once/src/main/java/com/sync/once/PersistedMap.java
@@ -0,0 +1,32 @@
+package com.sync.once;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by YH on 2016-11-03.
+ */
+
+public class PersistedMap {
+
+  private static final String DELIMITER = ",";
+
+  private SharedPreferences preferences;
+  private Map<String, List<Long>> map = new ConcurrentHashMap<>();
+  private final AsyncSharePreferenceLoader preferenceLoader;
+
+  public PersistedMap(Context context, String mapName) {
+    String preferenceName = "PersistedMap".concat(mapName);
+    preferenceLoader = new AsyncSharePreferenceLoader(context, preferenceName);
+  }
+
+  private void waitForLoad() {
+    if (preferenceLoader != null) return;
+
+    
+
+  }
+}
---
+++ common/once/src/main/java/com/sync/once/PersistedSet.java
@@ -0,0 +1,11 @@
+package com.sync.once;
+
+/**
+ * Created by YH on 2016-11-03.
+ */
+
+public class PersistedSet {
+
+
+
+}
