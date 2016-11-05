088a30db0ad2cebf00daaf43dcb122766abc60cc
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2016-11-05 12:24:47 +0800

    init Once by sync on 2016/11/05

--- common/once/src/main/java/com/sync/once/Once.java
+++ common/once/src/main/java/com/sync/once/Once.java
@@ -6,6 +6,9 @@ package com.sync.once;
 
 public class Once {
 
+  public static final int THIS_APP_INSTALL = 0;
+  public static final int THIS_APP_VERSION = 1;
+  public static final int THIS_APP_SESSION = 2;
 
-
+  private static long loastAppUpdateTime = -1;
 }
