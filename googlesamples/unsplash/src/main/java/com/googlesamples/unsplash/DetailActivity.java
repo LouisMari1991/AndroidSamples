625c27d658c0c89ed7e65f81e5a9e43617f05704
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2016-11-17 16:16:54 +0800

    sync on 2016/11/17

--- googlesamples/unsplash/src/main/java/com/googlesamples/unsplash/DetailActivity.java
+++ googlesamples/unsplash/src/main/java/com/googlesamples/unsplash/DetailActivity.java
@@ -1,12 +1,12 @@
 package com.googlesamples.unsplash;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Author：Administrator on 2016/8/31 0031 20:34
  * Contact：289168296@qq.com
  */
-public class DetailActivity extends Activity{
+public class DetailActivity extends AppCompatActivity{
 
 
 
