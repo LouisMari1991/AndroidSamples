d5240c7e044a9a25a6423c1d656a535551fbd5df
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-02-03 17:33:26 +0800

    2017/02/03

--- Androidkaifayishu/viewbase/src/main/java/com/sync/viewbase/MainActivity.java
+++ Androidkaifayishu/viewbase/src/main/java/com/sync/viewbase/MainActivity.java
@@ -1,12 +1,17 @@
 package com.sync.viewbase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.sync.viewbase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding mBinding;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
   }
 }
