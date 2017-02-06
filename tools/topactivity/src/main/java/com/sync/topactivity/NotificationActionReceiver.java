3d8878c18fc272c648da3882775b5b940c579875
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-02-06 23:15:09 +0800

    2017/02/06

--- tools/topactivity/src/main/java/com/sync/topactivity/NotificationActionReceiver.java
+++ tools/topactivity/src/main/java/com/sync/topactivity/NotificationActionReceiver.java
@@ -1,15 +1,53 @@
 package com.sync.topactivity;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Author：Administrator on 2017/2/5 0005 17:32
  * Contact：289168296@qq.com
  */
 public class NotificationActionReceiver extends BroadcastReceiver {
-  @Override public void onReceive(Context context, Intent intent) {
+  public static final int NOTIFICATION_ID = 1;
+  public static final String ACTION_NOTIFICATION_RECEIVER = "";
+  public static final int ACTION_PAUSE = 0;
+  public static final int ACTION_RESUME = 1;
+  public static final int ACTION_STOP = 2;
+  public static final String EXTRA_NOTIFICATION_ACTION = "command";
 
+  @Override public void onReceive(Context context, Intent intent) {
+    int command = intent.getIntExtra(EXTRA_NOTIFICATION_ACTION, -1);
+    switch (command) {
+      case ACTION_RESUME: {
+        break;
+      }
+      case ACTION_PAUSE: {
+        break;
+      }
+      case ACTION_STOP: {
+        break;
+      }
+    }
+  }
+
+  public static void showNotification(Context context, boolean isPause) {
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+  }
+
+  public static PendingIntent getPendingInent(Context context, int command) {
+    Intent intent = new Intent(ACTION_NOTIFICATION_RECEIVER);
+    intent.putExtra(EXTRA_NOTIFICATION_ACTION, command);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, command, intent, 0);
+    return pendingIntent;
+  }
+
+  public static void cancelNotification(Context context) {
+    NotificationManager nm =
+        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    nm.cancel(NOTIFICATION_ID);
   }
 }
--- tools/topactivity/src/main/java/com/sync/topactivity/WatchingAccessibilityService.java
+++ tools/topactivity/src/main/java/com/sync/topactivity/WatchingAccessibilityService.java
@@ -1,8 +1,39 @@
 package com.sync.topactivity;
 
+import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.view.accessibility.AccessibilityEvent;
+
 /**
  * Author：Administrator on 2017/2/5 0005 21:53
  * Contact：289168296@qq.com
  */
-public class WatchingAccessibilityService {
+public class WatchingAccessibilityService extends AccessibilityService {
+  private static WatchingAccessibilityService sInstance;
+
+  @Override public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+    if (SPHelper.isShowWindow(this)) {
+      TaskWindow.show(this,
+          accessibilityEvent.getPackageName() + "\n" + accessibilityEvent.getClassName());
+    }
+  }
+
+  @Override public void onInterrupt() {
+
+  }
+
+  @Override protected void onServiceConnected() {
+    super.onServiceConnected();
+  }
+
+  @Override public boolean onUnbind(Intent intent) {
+    sInstance = null;
+    TaskWindow.dismiss(this);
+    //NotificationActionReceiver
+    return super.onUnbind(intent);
+  }
+
+  public static WatchingAccessibilityService getInstance() {
+    return sInstance;
+  }
 }
