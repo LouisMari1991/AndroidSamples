f92231eeb359c8c9d3134a1c7206784b1f4dce2e
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-02-10 22:19:15 +0800

    2017/02/10

---
+++ Androidkaifayishu/viewbase/src/main/java/com/sync/viewbase/DemoActivity_2.java
@@ -0,0 +1,10 @@
+package com.sync.viewbase;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author：Administrator on 2017/2/10 0010 22:14
+ * Contact：289168296@qq.com
+ */
+public class DemoActivity_2 extends AppCompatActivity {
+}
--- Androidkaifayishu/viewbase/src/main/java/com/sync/viewbase/MainActivity.java
+++ Androidkaifayishu/viewbase/src/main/java/com/sync/viewbase/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sync.viewbase;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import com.sync.viewbase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+  }
+
+  public void onButtonClick(View v) {
+    Intent intent = new Intent();
+    if (v.getId() == R.id.button1) {
+      intent.setClass(this, TestActivity.class);
+    } else if (v.getId() == R.id.button2) {
+      intent.setClass(this, DemoActivity_1.class);
+    } else if (v.getId() == R.id.button3) {
+      intent.setClass(this, DemoActivity_2.class);
+    }
+    startActivity(intent);
   }
 }
