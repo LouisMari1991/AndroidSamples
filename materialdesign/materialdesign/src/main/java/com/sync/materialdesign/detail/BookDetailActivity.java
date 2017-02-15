898b059c7ddab98a90df7262d0551b333703f262
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-02-15 16:12:10 +0800

    2017/02/15

--- materialdesign/materialdesign/src/main/java/com/sync/materialdesign/detail/BookDetailActivity.java
+++ materialdesign/materialdesign/src/main/java/com/sync/materialdesign/detail/BookDetailActivity.java
@@ -1,5 +1,6 @@
 package com.sync.materialdesign.detail;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +8,10 @@ import android.support.v7.app.AppCompatActivity;
  * Contact：289168296@qq.com
  */
 public class BookDetailActivity extends AppCompatActivity {
+
+  private ViewPager mViewPager;
+  private Book mBook;
+
+
+
 }
