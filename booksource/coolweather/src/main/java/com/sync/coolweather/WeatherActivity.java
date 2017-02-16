0cfceb3f9f9ec43add04ef08e9964a164b549930
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-02-17 00:01:08 +0800

    2017/02/16

---
+++ booksource/coolweather/src/main/java/com/sync/coolweather/WeatherActivity.java
@@ -0,0 +1,13 @@
+package com.sync.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author：Administrator on 2017/2/16 0016 23:49
+ * Contact：289168296@qq.com
+ */
+public class WeatherActivity extends AppCompatActivity {
+
+
+
+}
---
+++ booksource/coolweather/src/main/java/com/sync/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.sync.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Author：Administrator on 2017/2/16 0016 23:39
+ * Contact：289168296@qq.com
+ */
+public class Province extends DataSupport {
+
+  private int id;
+  private String provinceName;
+  private int provinceCode;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getProvinceName() {
+    return provinceName;
+  }
+
+  public void setProvinceName(String provinceName) {
+    this.provinceName = provinceName;
+  }
+
+  public int getProvinceCode() {
+    return provinceCode;
+  }
+
+  public void setProvinceCode(int provinceCode) {
+    this.provinceCode = provinceCode;
+  }
+}
