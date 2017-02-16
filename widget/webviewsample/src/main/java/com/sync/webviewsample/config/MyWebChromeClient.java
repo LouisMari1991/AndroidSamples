38a79e6f09c7273b801f92909da94de550fd8088
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-02-16 22:09:16 +0800

    2017/02/16

--- widget/webviewsample/src/main/java/com/sync/webviewsample/config/MyWebChromeClient.java
+++ widget/webviewsample/src/main/java/com/sync/webviewsample/config/MyWebChromeClient.java
@@ -1,8 +1,11 @@
 package com.sync.webviewsample.config;
 
 import android.net.Uri;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import com.sync.webviewsample.WebViewActivity;
 
 /**
  * Author：Administrator on 2017/2/15 0015 22:30
@@ -12,6 +15,35 @@ public class MyWebChromeClient extends WebChromeClient {
 
   private ValueCallback<Uri> mUploadMessage;
   private ValueCallback<Uri[]> mUploadMessageForAndroid5;
+
+  public static int FILECHOOSE_RESULT_CODE = 1;
+  public static int FILECHOOSE_RESULT_CODE_FOR_ANDROID_5 = 2;
+
+  private View mXProgressVideo;
+  private WebViewActivity mActivity;
+  private IWebPageView mIWebPageView;
+  private View mXCustomView;
+  private CustomViewCallback mXCustomViewCallback;
+
+  public MyWebChromeClient(IWebPageView iWebPageView) {
+    mIWebPageView = iWebPageView;
+  }
+
+  @Override public void onShowCustomView(View view, CustomViewCallback callback) {
+    super.onShowCustomView(view, callback);
+  }
+
+  @Override public void onHideCustomView() {
+    super.onHideCustomView();
+  }
+
+  @Override public View getVideoLoadingProgressView() {
+    return super.getVideoLoadingProgressView();
+  }
+
+  @Override public void onProgressChanged(WebView view, int newProgress) {
+    super.onProgressChanged(view, newProgress);
+  }
 
 
 
