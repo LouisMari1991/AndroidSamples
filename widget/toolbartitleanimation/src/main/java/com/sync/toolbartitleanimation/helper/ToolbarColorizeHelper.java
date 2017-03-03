44264661c880ace3d65bcc8eaaedf6a39c382534
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-03-03 19:20:47 +0800

    2017/03/03

--- widget/toolbartitleanimation/src/main/java/com/sync/toolbartitleanimation/helper/ToolbarColorizeHelper.java
+++ widget/toolbartitleanimation/src/main/java/com/sync/toolbartitleanimation/helper/ToolbarColorizeHelper.java
@@ -3,6 +3,7 @@ package com.sync.toolbartitleanimation.helper;
 import android.app.Activity;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.support.v7.view.menu.ActionMenuItemView;
 import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -35,10 +36,23 @@ public class ToolbarColorizeHelper {
         for (int j = 0; j < ((ActionMenuView) v).getChildCount(); j++) {
           final View innerView = ((ActionMenuView) v).getChildAt(j);
           if (innerView instanceof ActionMenuView) {
-
+            for (int k = 0; k < ((ActionMenuItemView) innerView).getCompoundDrawables().length; k++) {
+              if (((ActionMenuItemView) innerView).getCompoundDrawables()[k] != null) {
+                final int finalK = k;
+                innerView.post(new Runnable() {
+                  @Override public void run() {
+                    ((ActionMenuItemView) innerView).getCompoundDrawables()[finalK].setColorFilter(colorFilter);
+                  }
+                });
+              }
+            }
           }
         }
       }
     }
+  }
+
+  private static void setOverflowButtonColor(final Activity activity, final PorterDuffColorFilter colorFilter) {
+
   }
 }
