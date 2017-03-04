6bcfecfc03248b263b016f63d470d110b8bc4e2b
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-03-04 16:48:36 +0800

    2017/03/04

--- tools/capturingphotos/src/main/java/com/sync/capturingphotos/MainActivity.java
+++ tools/capturingphotos/src/main/java/com/sync/capturingphotos/MainActivity.java
@@ -199,6 +199,29 @@ public class MainActivity extends AppCompatActivity {
     }
     galleryAddPic(srcPath);
 
+    //// Get the dimensions of the View
+    //int targetW = mImageView.getWidth();
+    //int targetH = mImageView.getHeight();
+    //
+    //// Get the dimensions of the bitmap
+    //BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+    //bmOptions.inJustDecodeBounds = true;
+    //BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    //int photoW = bmOptions.outWidth;
+    //int photoH = bmOptions.outHeight;
+    //
+    //// Determine how much to scale down the image
+    //int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+    //
+    //// Decode the image file into a Bitmap sized to fill the View
+    //bmOptions.inJustDecodeBounds = false;
+    //bmOptions.inSampleSize = scaleFactor;
+    //bmOptions.inPurgeable = true;
+    //
+    //Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    //mImageView.setImageBitmap(bitmap);
+    //break;
+
   }
 
   @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
