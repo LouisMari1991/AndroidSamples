ed40ae8453d3afb9e31705e0f1d0a2fcc54d8afa
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-03-09 23:41:26 +0800

    2017/03/09

--- materialdesign/materialdesign/src/main/java/com/sync/materialdesign/detail/BookDetailActivity.java
+++ materialdesign/materialdesign/src/main/java/com/sync/materialdesign/detail/BookDetailActivity.java
@@ -1,7 +1,12 @@
 package com.sync.materialdesign.detail;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import com.sync.materialdesign.R;
 
 /**
  * Author：Administrator on 2017/1/15 0015 15:28
@@ -12,6 +17,16 @@ public class BookDetailActivity extends AppCompatActivity {
   private ViewPager mViewPager;
   private Book mBook;
 
-
-
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_appbar_detail);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        onBackPressed();
+      }
+    });
+  }
 }
