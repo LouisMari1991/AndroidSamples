898c4f10e8348a3a44ca7b317e459139d3df93ba
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-03-10 21:05:05 +0800

    2016/03/10

--- MaterialDesign/materialdesign/src/main/java/com/sync/materialdesign/list/ItemDetailActivity.java
+++ MaterialDesign/materialdesign/src/main/java/com/sync/materialdesign/list/ItemDetailActivity.java
@@ -1,8 +1,18 @@
 package com.sync.materialdesign.list;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.sync.materialdesign.R;
+
 /**
  * Author：Administrator on 2017/2/14 0014 23:47
  * Contact：289168296@qq.com
  */
-public class ItemDetailActivity {
+public class ItemDetailActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_item_detail);
+  }
 }
