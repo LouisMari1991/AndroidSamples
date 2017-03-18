c14a210aaca1c291642e1fa1335253785cb3575f
Marishunxiang@gmail.com <Marishunxiang@gmail.com>
2017-03-18 14:50:53 +0800

    2016/03/18

---
+++ widget/toolbartitleanimation/src/main/java/com/sync/toolbartitleanimation/ContentFragment.java
@@ -0,0 +1,43 @@
+package com.sync.toolbartitleanimation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Author：Administrator on 2017/3/18 0018 14:45
+ * Contact：289168296@qq.com
+ */
+public class ContentFragment extends Fragment {
+
+  private static final String TITLE = "title";
+
+  private String mTitle;
+
+  private static ContentFragment newInstance(String title) {
+    ContentFragment fragment = new ContentFragment();
+    Bundle b = new Bundle();
+    b.putString(TITLE, title);
+    fragment.setArguments(b);
+    return fragment;
+  }
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      mTitle = getArguments().getString(TITLE);
+    }
+  }
+
+  @Nullable @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View root = inflater.inflate(R.layout.fragment_content, container, false);
+    TextView textTitle = (TextView) root.findViewById(R.id.title_text);
+    textTitle.setText(mTitle);
+    return root;
+  }
+}
--- widget/toolbartitleanimation/src/main/java/com/sync/toolbartitleanimation/ui/CircleIndicator.java
+++ widget/toolbartitleanimation/src/main/java/com/sync/toolbartitleanimation/ui/CircleIndicator.java
@@ -1,6 +1,7 @@
 package com.sync.toolbartitleanimation.ui;
 
 import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.AnimatorRes;
@@ -8,6 +9,8 @@ import android.support.annotation.DrawableRes;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.LinearLayout;
 import com.sync.toolbartitleanimation.R;
 
@@ -19,7 +22,7 @@ public class CircleIndicator extends LinearLayout implements ViewPager.OnPageCha
 
   private final int DEFAULT_INDICATOR_WIDTH = 5;
 
-  private ViewPager mViewPager;
+  private ViewPager mViewpager;
   private int mIndicatorMargin = -1;
   private int mIndicatorWidth = -1;
   private int mIndicatorHeight = -1;
@@ -28,8 +31,8 @@ public class CircleIndicator extends LinearLayout implements ViewPager.OnPageCha
   private int mIndicatorBackgroundResId = R.drawable.white_radius;
   private int mIndicatorUnselectedBackgroundResId = R.drawable.white_radius;
   private int mCurrentPosition = 0;
-  private Animator mAnimatorOut;
-  private Animator mAnimatorIn;
+  private Animator mAnimationOut;
+  private Animator mAnimationIn;
 
   public CircleIndicator(Context context) {
     super(context);
@@ -61,13 +64,12 @@ public class CircleIndicator extends LinearLayout implements ViewPager.OnPageCha
   }
 
   public void configureIndicator(int indicatorWidth, int indicatorHeight, int indicatorMargin) {
-    configureIndicator(indicatorWidth, indicatorHeight, indicatorMargin,
-        R.animator.scale_with_alpha, 0, R.drawable.white_radius, R.drawable.white_radius);
+    configureIndicator(indicatorWidth, indicatorHeight, indicatorMargin, R.animator.scale_with_alpha, 0,
+        R.drawable.white_radius, R.drawable.white_radius);
   }
 
   public void configureIndicator(int indicatorWidth, int indicatorHeight, int indicatorMargin,
-      @AnimatorRes int animatorId, @AnimatorRes int animatorReverseId,
-      @DrawableRes int indicatorBackgroundId,
+      @AnimatorRes int animatorId, @AnimatorRes int animatorReverseId, @DrawableRes int indicatorBackgroundId,
       @DrawableRes int indicatorUnselectedBackgroundId) {
     mIndicatorWidth = indicatorWidth;
     mIndicatorHeight = indicatorHeight;
@@ -82,26 +84,101 @@ public class CircleIndicator extends LinearLayout implements ViewPager.OnPageCha
   }
 
   private void checkIndicatorConfig(Context context) {
+    mIndicatorWidth = (mIndicatorWidth < 0) ? dip2px(DEFAULT_INDICATOR_WIDTH) : mIndicatorWidth;
+    mIndicatorHeight = (mIndicatorHeight < 0) ? dip2px(DEFAULT_INDICATOR_WIDTH) : mIndicatorHeight;
+    mIndicatorMargin = (mIndicatorMargin < 0) ? dip2px(DEFAULT_INDICATOR_WIDTH) : mIndicatorMargin;
 
+    mAnimatorResId = (mAnimatorResId == 0) ? R.animator.scale_with_alpha : mAnimatorResId;
+    mAnimationOut = AnimatorInflater.loadAnimator(context, mAnimatorResId);
+    if (mAnimatorResId == 0) {
+      mAnimationIn = AnimatorInflater.loadAnimator(context, mAnimatorResId);
+      mAnimationIn.setInterpolator(new ReverseInterpolator());
+    } else {
+      mAnimationIn = AnimatorInflater.loadAnimator(context, mAnimatorReverseResId);
+    }
+    mIndicatorBackgroundResId = (mIndicatorBackgroundResId == 0) ? R.drawable.white_radius : mIndicatorBackgroundResId;
+    mIndicatorUnselectedBackgroundResId =
+        (mIndicatorUnselectedBackgroundResId == 0) ? mIndicatorBackgroundResId : mIndicatorUnselectedBackgroundResId;
   }
 
-  @Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+  public void setViewPager(ViewPager viewPager) {
+    mViewpager = viewPager;
+    mCurrentPosition = mViewpager.getCurrentItem();
+    createIndicators(viewPager);
+    mViewpager.removeOnPageChangeListener(this);
+    mViewpager.addOnPageChangeListener(this);
+    onPageSelected(mCurrentPosition);
+  }
+
+  @Deprecated @Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
   }
 
   @Override public void onPageSelected(int position) {
+    if (mViewpager.getAdapter() == null || mViewpager.getAdapter().getCount() <= 0) {
+      return;
+    }
 
+    View currentIndicator = getChildAt(mCurrentPosition);
+    currentIndicator.setBackgroundResource(mIndicatorUnselectedBackgroundResId);
+    mAnimationIn.setTarget(currentIndicator);
+    mAnimationIn.start();
+
+    View selectorIndicator = getChildAt(position);
+    selectorIndicator.setBackgroundResource(mIndicatorBackgroundResId);
+    mAnimationOut.setTarget(selectorIndicator);
+    mAnimationOut.start();
+
+    mCurrentPosition = position;
   }
 
   @Override public void onPageScrollStateChanged(int state) {
+  }
 
+  private void createIndicators(ViewPager viewpager) {
+    removeAllViews();
+    if (viewpager.getAdapter() == null) {
+      return;
+    }
+
+    int count = viewpager.getAdapter().getCount();
+    if (count <= 0) {
+      return;
+    }
+    // addIndicator(mIndicatorBackgroundResId, mAnimationOut);
+    for (int i = 0; i < count; i++) {
+      addIndicator(mIndicatorUnselectedBackgroundResId, mAnimationIn);
+    }
+  }
+
+  private void addIndicator(@DrawableRes int backgroundDrawableId, Animator animator) {
+    if (animator.isRunning()) {
+      animator.end();
+    }
+
+    View Indicator = new View(getContext());
+    Indicator.setBackgroundResource(backgroundDrawableId);
+    addView(Indicator, mIndicatorWidth, mIndicatorHeight);
+    LayoutParams lp = (LayoutParams) Indicator.getLayoutParams();
+    lp.leftMargin = mIndicatorMargin;
+    lp.rightMargin = mIndicatorMargin;
+    Indicator.setLayoutParams(lp);
+
+    animator.setTarget(Indicator);
+    animator.start();
+  }
+
+  private class ReverseInterpolator implements Interpolator {
+
+    @Override public float getInterpolation(float v) {
+      return Math.abs(1.0f - v);
+    }
   }
 
   public int dip2px(float dpValue) {
     final float scale = getResources().getDisplayMetrics().density;
     return (int) (dpValue * scale + 0.5f);
   }
-
 }
 
 
