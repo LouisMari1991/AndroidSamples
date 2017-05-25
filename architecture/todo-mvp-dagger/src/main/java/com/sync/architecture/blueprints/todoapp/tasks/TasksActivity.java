91225532c952c80e1ef9362f11945b5cfc6c7c14
MariShunxiang <Marishunxiang@gmail.com>
2017-05-25 21:19:37 +0800

    add mvp-dagger

---
+++ architecture/todo-mvp-dagger/src/main/java/com/sync/architecture/blueprints/todoapp/tasks/TasksActivity.java
@@ -0,0 +1,16 @@
+package com.sync.architecture.blueprints.todoapp.tasks;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description:
+ * Author：SYNC on 2017/5/25 0025 20:23
+ * Contact：289168296@qq.com
+ */
+public class TasksActivity extends AppCompatActivity {
+
+  private static final String CURRENT_FILTERING_KEY = "CURRENT_FILTERING_KEY";
+
+
+
+}
