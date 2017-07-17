98c64f4d81de3041d6d63787a664f58a0cf6c98e
MariShunxiang <marishunxiang@gmail.com>
2017-07-17 23:16:08 +0800

    topeka

--- googlesamples/topeka/src/main/java/com/googlesamples/topeka/adapter/ScoreAdapter.java
+++ googlesamples/topeka/src/main/java/com/googlesamples/topeka/adapter/ScoreAdapter.java
@@ -1,9 +1,14 @@
 package com.googlesamples.topeka.adapter;
 
 import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.googlesamples.topeka.R;
 import com.googlesamples.topeka.model.Category;
 import com.googlesamples.topeka.model.quiz.Quiz;
 import java.util.List;
@@ -29,18 +34,52 @@ public class ScoreAdapter extends BaseAdapter {
   }
 
   @Override public int getCount() {
-    return 0;
+    return count;
   }
 
-  @Override public Object getItem(int position) {
-    return null;
+  @Override public Quiz getItem(int position) {
+    return mQuizList.get(position);
   }
 
   @Override public long getItemId(int position) {
-    return 0;
+    if (position > count || position < count) {
+      return AbsListView.INVALID_POSITION;
+    }
+    return position;
   }
 
   @Override public View getView(int position, View convertView, ViewGroup parent) {
-    return null;
+    if (null == convertView) {
+      convertView = createView(parent);
+    }
+
+    final Quiz quiz = getItem(position);
+    ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+    viewHolder.mQuizView.setText(quiz.getQuestion());
+    viewHolder.mAnswerView.setText(quiz.getStringAnswer());
+    //setSolvedStateForQuiz(viewHolder.mSolvedState, position);
+    return convertView;
+  }
+
+  private View createView(ViewGroup parent) {
+    View convertView;
+    final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+    ViewGroup scorecardItem = (ViewGroup) inflater.inflate(R.layout.item_scorecard, parent, false);
+    convertView = scorecardItem;
+    ViewHolder holder = new ViewHolder(scorecardItem);
+    convertView.setTag(holder);
+    return convertView;
+  }
+
+  private class ViewHolder {
+    final TextView  mAnswerView;
+    final TextView  mQuizView;
+    final ImageView mSolvedState;
+
+    public ViewHolder(ViewGroup scorecardItem) {
+      mAnswerView = (TextView) scorecardItem.findViewById(R.id.quiz);
+      mQuizView = (TextView) scorecardItem.findViewById(R.id.answer);
+      mSolvedState = (ImageView) scorecardItem.findViewById(R.id.solved_state);
+    }
   }
 }
