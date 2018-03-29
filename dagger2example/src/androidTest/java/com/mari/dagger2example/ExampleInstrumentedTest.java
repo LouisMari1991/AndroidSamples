a1b9b27a3b05db40fd7d3536fd261e649b2d571a
Mari <Marishunxiang@gmail.com>
2018-03-29 22:18:20 +0800

    fix

--- dagger2example/src/androidTest/java/com/mari/dagger2example/ExampleInstrumentedTest.java
+++ dagger2example/src/androidTest/java/com/mari/dagger2example/ExampleInstrumentedTest.java
@@ -14,8 +14,10 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class) public class ExampleInstrumentedTest {
-  @Test public void useAppContext() throws Exception {
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+  @Test
+  public void useAppContext() {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
